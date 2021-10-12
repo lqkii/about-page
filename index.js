75e89edf501889f46c2cba2757c36bd307677e41
lqkii <chowl801@newschool.edu>
2021-10-11 23:17:46 -0400

    GSAP animations without reversal/scrub, and added 2nd page

---
+++ index.js
@@ -0,0 +1,86 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("header", {
+    y: 50,
+    opacity: 1,
+    duration: 1.5,
+    ease: "expo.OutOut"
+});
+
+gsap.from("#arrow", {
+    y: -50,
+    opacity: 0,
+    duration: 1.5,
+    ease: "power1.OutOut",
+    delay: 0.8
+});
+
+gsap.from("#div-insta", {
+    y: -50,
+    opacity: 0,
+    duration: 1.5,
+    ease: "power2.OutOut",
+    delay: 0.4
+});
+
+gsap.to("#aboutme", {
+    scrollTrigger: "#aboutme",
+    y: -200,
+    opacity: 100,
+    duration: 1.5,
+    ease: "expo.OutOut"
+});
+
+gsap.to("#hk", {
+    scrollTrigger: "#aboutme",
+    y: -200,
+    opacity: 100,
+    duration: 1.5,
+    ease: "expo.OutOut"
+});
+
+gsap.to("#arrow", {
+    scrollTrigger: "#aboutme",
+    y: -250,
+    opacity: 100,
+    duration: 1.5,
+    ease: "expo.OutOut"
+});
+
+gsap.to("#div-insta", {
+    scrollTrigger: {
+        trigger:  "#aboutme",
+        markers: true
+    },
+    y: -438,
+    opacity: 100,
+    duration: 1.5,
+    ease: "expo.OutOut"
+});
+
+gsap.to("#title", {
+    scrollTrigger: {
+        trigger:  "#aboutme",
+        scrub: 1
+    },
+    y: -1200,
+    opacity: 1,
+    duration: 2,
+    ease: "power1.OutOut"
+});
+
+gsap.to("#title2", {
+    scrollTrigger: {
+        trigger:  "#aboutme",
+        scrub: 1
+    },
+    y: -1200,
+    opacity: 1,
+    duration: 2,
+    ease: "power1.OutOut"
+});
+
+
+barba.init({
+    // ...
+  })
